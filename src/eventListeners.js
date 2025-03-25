5cd3c133d37432a91b466f0b82667f693194cfc0
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-03-25 22:55:14 +0530

    fix issue with adding default project as favourite

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,10 +1,12 @@
 import { toggleHeader, addToFavorites, removeFromFavorites, buildProjectList, toggleAddProjectModal, addTask } from "./display";
 import { addProject } from "./projects";
 
+import addFavourites_icon from "./images/favourites_add_icon.png";
+import favouritesAdded_icon from "./images/favourites_added_icon.png";
+
 export default function attachListeners(element = "") {
     const expandableHeaders = document.querySelectorAll(".expandable");
     const addProjectBtn = document.querySelector("#add-project-icon");
-    const projectLists = [...document.querySelector("#projects-list").firstElementChild.children];
     const projectName = document.querySelector("#project-name");
     const addTaskBtn = document.querySelector("#add-task-icon");
     
@@ -13,15 +15,7 @@ export default function attachListeners(element = "") {
         switch (element.id) {
             case "add-project-btn":
                 element.addEventListener("click", () => {
-                    let projects = [];
-            
-                    if (projectLists.length > 0) {
-                        projectLists.forEach(project => {
-                            projects.push(project.firstElementChild.innerHTML);
-                        });
-                    }
-            
-                    projects.push(projectName.value.trim());
+                    let projects = getProjectList();
             
                     buildProjectList(projects)
                     projectName.value = "";
@@ -49,6 +43,8 @@ export default function attachListeners(element = "") {
         }        
     }
     else {
+        const defaultFavIcon = document.querySelector("#add-fav-icon");
+
         expandableHeaders.forEach(header => {
             header.addEventListener("click", (event) => {
                toggleHeader(event.target)
@@ -61,6 +57,17 @@ export default function attachListeners(element = "") {
         })
 
         addTaskBtn.addEventListener("click", createNewTask);
+        
+        defaultFavIcon.addEventListener("click", (event) => {
+            if (defaultFavIcon.src === addFavourites_icon) {
+                defaultFavIcon.src = favouritesAdded_icon;
+                addToFavorites(event.target.previousElementSibling);
+            }
+            else {
+                defaultFavIcon.src = addFavourites_icon;
+                removeFromFavorites(event.target.previousElementSibling);
+            }
+        })
     }
 }
 
@@ -79,4 +86,21 @@ function createNewTask() {
         
     }
     addTask(tasks)  
+}
+
+function getProjectList() {
+    const projectName = document.querySelector("#project-name");
+    const projectLists = [...document.querySelector("#projects-list").firstElementChild.children];
+
+    let projects = [];
+
+    if (projectLists.length > 0) {
+        projectLists.forEach(project => {
+            projects.push(project.firstElementChild.innerHTML);
+        });
+    }
+    
+    projects.push(projectName.value.trim());
+    
+    return projects;
 }
